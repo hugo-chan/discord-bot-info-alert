0f300e9a8da4e4c57e0be7d85dc63724e3ce1c3b
Hugo Chan <hugoyhc14@gmail.com>
2020-08-15 23:51:23 +0800

    get to message's channel

--- src/commands/get.js
+++ src/commands/get.js
@@ -1,17 +1,17 @@
-function get(client) {
-    const { subscriptions, channel_id } = require("../../config.json");
+function get(msg) {
+    const { subscriptions } = require("../../config.json");
     const { db_wrapper, get_info } = require("../db.js");
     const { generate_embed } = require("../util.js");
 
     db_wrapper(get_info, subscriptions).then((res) => JSON.stringify(res))
         .then((res) => {
-            const msg = generate_embed(res, "now");
-            client.channels.cache.get(channel_id).send(msg);
+            const send_msg = generate_embed(res, "now");
+            msg.channel.send(send_msg);
         });
 }
 
 module.exports = {
     name: "get",
     description: "gets info for your subscriptions now",
-    execute: (client) => get(client),
+    execute: (client, msg) => get(msg),
 };
